dab86633d5d1ca68d611ba7fbebfbd18e3dcf005
Đào Minh Khôi <minhkhoidao18@gmail.com>
2026-04-10 23:31:12 +0700

    fix: add H2 for tests, test profile config, fix SERVER_PORT missing in test context

--- src/test/java/com/codek/movieauthservice/MovieAuthServiceApplicationTests.java
+++ src/test/java/com/codek/movieauthservice/MovieAuthServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.codek.movieauthservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MovieAuthServiceApplicationTests {
 
     @Test
